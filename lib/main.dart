cf453f06066543edd2452700ac06d2e0e813b3bb
sasaibrahim <mostafaelalamy811@gmail.com>
2023-10-19 18:04:22 +0200

    wheather app

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:whether_app/pages/home/home_screen.dart';
+import 'package:whether_app/utils/binding/HomeBinding.dart';
+
+//import 'package:weather_app/pages/home/home_screen.dart';
+//import 'package:weather_app/utils/Binding/HomeBinding.dart';
+
+void main() {
+  runApp(WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      getPages: [
+        GetPage(
+          name: '/',
+          page: () => HomeScreen(),
+          binding: HomeBinding(),
+        )
+      ],
+    );
+  }
+}
